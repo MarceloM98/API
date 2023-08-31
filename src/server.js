0ff29cc633bc085ec1e7a28f62cdbe0a07d3c371
Marcelo Martins <marcelocmartins98@gmail.com>
2023-08-31 17:46:14 -0300

    sadas

--- src/server.js
+++ src/server.js
@@ -1,23 +1,28 @@
 require("express-async-errors");
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
+const uploadConfig = require("./configs/upload");
 
+const cors = require("cors");
 const express = require("express"); //importando o express
 const routes = require("./routes");
 
 migrationsRun();
 
 const app = express(); //inicializando o express
+app.use(cors());
 app.use(express.json());
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({
-        status: "error",
-        message: error.message
-      });
+      status: "error",
+      message: error.message,
+    });
   }
 
   console.error(error);
